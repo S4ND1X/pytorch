49e55fe32ad297cd603c6aa0f0409c619cff573b
fduwjj <fduwjj@fb.com>
2021-11-17 22:52:52 -0800

    [PyTorch][3/N] Enable the rest forward spec options for ShardedEmbedding and ShardedEmbeddingBag.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/67799
    
    We have enabled the sharding embedding and embedding bag in https://github.com/pytorch/pytorch/pull/67188 and https://github.com/pytorch/pytorch/pull/66604. We now want to enable as many parameters as defined in doc as possible: https://pytorch.org/docs/stable/generated/torch.nn.functional.embedding_bag.html, https://pytorch.org/docs/stable/generated/torch.nn.functional.embedding.html.
    
    For the ones that we don't support we just throw exception.
    
    Last but not least, we use get to get params instead of directly using the key.
    ghstack-source-id: 143715924
    
    Differential Revision: [D31985333](https://our.internmc.facebook.com/intern/diff/D31985333/)

--- test/distributed/_sharded_tensor/ops/test_embedding.py
+++ test/distributed/_sharded_tensor/ops/test_embedding.py
@@ -15,6 +15,7 @@ from torch.testing._internal.common_utils import (
     run_tests,
 )
 from torch.testing._internal.distributed._sharded_tensor import (
+    TEST_GPU_NUM,
     ShardedTensorTestBase,
     with_comms,
 )
@@ -31,17 +32,38 @@ if TEST_WITH_DEV_DBG_ASAN:
 
 
 class TestShardedEmbedding(ShardedTensorTestBase):
-    def _run_sharded_embedding(self, spec, input_size, num_embeddings, embedding_dim):
+    def _run_sharded_embedding(
+        self,
+        spec,
+        input_size,
+        num_embeddings,
+        embedding_dim,
+        max_norm=None,
+        norm_type=2.0,
+        padding_idx=None,
+    ):
         # Use same seed.
         torch.manual_seed(0)
-        local_embedding = torch.nn.Embedding(num_embeddings, embedding_dim).cuda(
-            self.rank
+        local_embedding = torch.nn.Embedding(
+            num_embeddings,
+            embedding_dim,
+            max_norm=max_norm,
+            norm_type=norm_type,
+            padding_idx=padding_idx,
+        ).cuda(self.rank)
+
+        sharded_embedding = torch.nn.Embedding(
+            num_embeddings,
+            embedding_dim,
+            max_norm=max_norm,
+            norm_type=norm_type,
+            padding_idx=padding_idx,
         )
 
-        sharded_embedding = torch.nn.Embedding(num_embeddings, embedding_dim)
-
         # Copy the weights from local embedding
-        sharded_embedding.weight = local_embedding.weight
+        sharded_embedding.weight = torch.nn.Parameter(
+            local_embedding.weight.detach().clone()
+        )
 
         # Shard the parameter.
         shard_parameter(sharded_embedding, "weight", spec)
@@ -58,13 +80,25 @@ class TestShardedEmbedding(ShardedTensorTestBase):
         self.assertEqual(local_output, sharded_output)
 
         # Validate for torch.nn.functional.embedding version.
-        local_output = torch.nn.functional.embedding(inp, local_embedding.weight)
-        sharded_output = torch.nn.functional.embedding(inp, sharded_embedding.weight)
+        local_output = torch.nn.functional.embedding(
+            inp,
+            local_embedding.weight,
+            max_norm=max_norm,
+            norm_type=norm_type,
+            padding_idx=padding_idx,
+        )
+        sharded_output = torch.nn.functional.embedding(
+            inp,
+            sharded_embedding.weight,
+            max_norm=max_norm,
+            norm_type=norm_type,
+            padding_idx=padding_idx,
+        )
 
         self.assertEqual(local_output, sharded_output)
 
     @with_comms(init_rpc=False)
-    @skip_if_lt_x_gpu(4)
+    @skip_if_lt_x_gpu(TEST_GPU_NUM)
     @requires_nccl()
     def test_sharded_embedding_colwise(self):
         for spec in generate_chunk_sharding_specs_for_test(1):
@@ -73,9 +107,16 @@ class TestShardedEmbedding(ShardedTensorTestBase):
             self._run_sharded_embedding(spec, [8, 6, 5, 4], 23, 13)
             self._run_sharded_embedding(spec, [8, 6, 5, 4, 7], 23, 16)
             self._run_sharded_embedding(spec, [4], 15, 14)
+            self._run_sharded_embedding(spec, [34], 15, 14, padding_idx=10)
+            self._run_sharded_embedding(spec, [8, 6, 5, 4], 23, 13, padding_idx=12)
+            self._run_sharded_embedding(spec, [4, 5, 6], 23, 13, max_norm=2.5)
+            self._run_sharded_embedding(
+                spec, [8, 16], 12, 12, max_norm=1.25, norm_type=1.0
+            )
+            self._run_sharded_embedding(spec, [30], 15, 14, max_norm=2.0)
 
     @with_comms(init_rpc=False)
-    @skip_if_lt_x_gpu(4)
+    @skip_if_lt_x_gpu(TEST_GPU_NUM)
     @requires_nccl()
     def test_sharded_embedding_rowwise(self):
         for spec in generate_chunk_sharding_specs_for_test(0):
@@ -83,11 +124,24 @@ class TestShardedEmbedding(ShardedTensorTestBase):
             self._run_sharded_embedding(spec, [5, 12], 16, 22)
             self._run_sharded_embedding(spec, [5, 4], 32, 12)
             self._run_sharded_embedding(spec, [6, 7, 6], 64, 11)
+            self._run_sharded_embedding(spec, [5, 12], 16, 22, max_norm=2.5)
+            self._run_sharded_embedding(spec, [6, 7, 6], 64, 11, padding_idx=30)
+            self._run_sharded_embedding(spec, [6, 5, 3], 26, 11, max_norm=2.0)
 
             # Test uneven split.
             self._run_sharded_embedding(spec, [8, 6, 5, 4], 19, 11)
             self._run_sharded_embedding(spec, [6, 7, 6], 21, 11)
             self._run_sharded_embedding(spec, [4], 21, 11)
+            self._run_sharded_embedding(spec, [8, 6, 5, 4], 21, 11, padding_idx=10)
+            self._run_sharded_embedding(
+                spec,
+                [6, 16],
+                27,
+                11,
+                max_norm=2.0,
+            )
+            self._run_sharded_embedding(spec, [4], 14, 11, max_norm=2.5)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     run_tests()
